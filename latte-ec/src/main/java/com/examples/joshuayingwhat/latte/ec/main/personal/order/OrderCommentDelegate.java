d5d4033f1dadf17409a773efc2d9f66c60d20e2f
JoshuaYingWhatEgr <17721354011@163.com>
2019-10-25 12:03:55 +0800

    完成晒单评分和图片功能

--- latte-ec/src/main/java/com/examples/joshuayingwhat/latte/ec/main/personal/order/OrderCommentDelegate.java
+++ latte-ec/src/main/java/com/examples/joshuayingwhat/latte/ec/main/personal/order/OrderCommentDelegate.java
@@ -1,5 +1,6 @@
 package com.examples.joshuayingwhat.latte.ec.main.personal.order;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -13,6 +14,10 @@ import androidx.appcompat.widget.Toolbar;
 import com.examples.joshuayingwhat.latte.delegates.LatteDelegate;
 import com.examples.joshuayingwhat.latte.ec.R;
 import com.examples.joshuayingwhat.latte.ec.R2;
+import com.examples.joshuayingwhat.latte.utils.callback.CallBackManager;
+import com.examples.joshuayingwhat.latte.utils.callback.CallBackType;
+import com.examples.joshuayingwhat.latte.utils.callback.IGlobalCallBack;
+import com.joshuayingwhat.latte_ui.ui.widget.AutoPhotoLayout;
 import com.joshuayingwhat.latte_ui.ui.widget.StartLayout;
 
 import butterknife.BindView;
@@ -37,6 +42,8 @@ public class OrderCommentDelegate extends LatteDelegate {
     StartLayout mStarLayout = null;
     @BindView(R2.id.et_order_comment)
     AppCompatEditText etOrderComment;
+    @BindView(R2.id.custom_auto_photo_layout)
+    AutoPhotoLayout mAutoPhotoLayout;
 
     @Override
     public Object setLayout() {
@@ -45,13 +52,20 @@ public class OrderCommentDelegate extends LatteDelegate {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
-
+        mAutoPhotoLayout.setDelegate(this);
+        CallBackManager.getInstance().addCallBack(CallBackType.ON_CROP, new IGlobalCallBack<Uri>() {
+            @Override
+            public void executeCallBack(@Nullable Uri uri) {
+                mAutoPhotoLayout.onCropTarget(uri);
+            }
+        });
     }
 
     @OnClick(R2.id.top_tv_comment_commit)
     void onClickSubmit() {
 
     }
+
 }
 
 
